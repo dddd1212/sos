8fcbcd3d93e93b9b5f753f188571c0251a650651
dddd1212 <dddd1212@gmail.com>
2016-03-12 23:57:52 +0200

    real_mode.asm added

---
+++ stage1/make.py
@@ -0,0 +1,39 @@
+import os
+
+######### General configuration ########
+RAW_DISK_FILE_PATH = os.path.join("output","sos.bin")
+VMDK_DISK_FILE_PATH = os.path.join("output","sos.vmdk")
+
+
+######### BOOT LOADER CONFIGURATION #################
+COMPILE_BOOTLOADER = True
+REAL_MODE_ASM_PATH = os.path.join("tools","nasm","nasm.exe")
+REAL_MODE_FILE_PATH = os.path.join("bootloader", "real_mode.asm")
+REAL_MODE_OUTPUT = os.path.join("bootloader", "real_mode.bin")
+#####################################################
+
+######### Glue configuration ###########
+GLUE_ALL = True
+########################################
+
+###### RAW to VMDK ###########
+RAW_TO_VMDK = True
+RAW_TO_VMDK_TOOL_PATH = os.path.join("tools","raw2vmdk","VBoxManage.exe")
+##############################
+
+
+def main():
+    if COMPILE_BOOTLOADER:
+        os.system(REAL_MODE_ASM_PATH + " " + REAL_MODE_FILE_PATH + " -o " + REAL_MODE_OUTPUT)
+    
+    if GLUE_ALL:
+        open(RAW_DISK_FILE_PATH,"wb").write(open(REAL_MODE_OUTPUT,"rb").read())
+    
+    
+    if RAW_TO_VMDK:
+        os.system(RAW_TO_VMDK_TOOL_PATH + " convertfromraw %s %s --format VMDK"%(RAW_DISK_FILE_PATH, VMDK_DISK_FILE_PATH))
+    
+    
+    
+    
+main()
