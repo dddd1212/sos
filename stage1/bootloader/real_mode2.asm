d3a27917a2824660274c28ab6747d6e773a38f6c
dddd1212 <dddd1212@gmail.com>
2016-03-18 02:55:34 +0200

    added 64 bit and paging

--- stage1/bootloader/real_mode2.asm
+++ stage1/bootloader/real_mode2.asm
@@ -34,6 +34,9 @@ Main:
 	; set the display to VGA text mode now
 	; because interrupts must be disabled
 	;
+    mov ax, 0x2401
+    int 0x15 ; enable A20 line
+    
     mov si, here1
     call print_string
 	mov ax,3
@@ -90,9 +93,70 @@ prot_mode:
 	;
 	; write a status message
 	;
-	mov ebx,0xB8000 ; address of first char for VGA mode 3
+    
+    mov eax, 0x100003
+    mov ebx,0x100F68
+    mov [ebx],eax ; set the PXE point to itself
+    
+    mov eax, 0x100000
+    mov cr3, eax ; set cr3 point to the PXE root
+    
+    mov eax, 0x101003
+    mov ebx,0x100000
+    mov [ebx],eax ; set the PXE entry of code
+    
+    mov eax, 0x102003
+    mov ebx, 0x101000
+    mov [ebx],eax ; set the PPE entry of code
+    
+    mov eax, 0x103003
+    mov ebx, 0x102000
+    mov [ebx],eax ; set the PDE entry of code
+    
+    mov eax, 0x7003
+    mov ebx, 0x103038
+    mov [ebx],eax ; set the PDE entry of code
+    
+    mov eax, cr4                 ; Set the A-register to control register 4.
+    or eax, 1 << 5               ; Set the PAE-bit, which is the 6th bit (bit 5).
+    mov cr4, eax                 ; Set control register 4 to the A-register.
+    
+    mov ecx, 0xC0000080          ; Set the C-register to 0xC0000080, which is the EFER MSR.
+    rdmsr                        ; Read from the model-specific register.
+    or eax, 1 << 8               ; Set the LM-bit which is the 9th bit (bit 8).
+    wrmsr                        ; Write to the model-specific register.
+    
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+    
+    inc eax
+    lgdt [GDT64.Pointer]
+    jmp GDT64.Code:mode64
+bits 64
+mode64:
+    mov rax, 0xFFFFF6FB7DBEDFF0 ; pxe
+    mov qword [rax], 0x104003
+    ;invlpg [0xFFFFF6FB7DBFE000] ; ppe
+    
+    mov rax, 0xFFFFF6FB7DBFE000 ; ppe
+    mov qword [rax],0x105003
+    ;invlpg [0xFFFFF6FB7FC00000] ; pde
+    
+    mov rax, 0xFFFFF6FB7FC00000 ; pde
+    mov qword [rax],0x106003
+    ;invlpg [0xFFFFF6FF80000000] ; pte
+    
+    mov rax, 0xFFFFF6FF80000000 ; pte
+    mov qword [rax], 0xB8003 ; screen
+    ;invlpg [0xffffff0000000000] ; pte
+    
+    
+    
+	mov rbx,0xffffff0000000000 ; address of first char for VGA mode 3
+    mov word [rbx],0x0f44
 	;
-	mov esi,TextProtectedMode ; si = message text
+	mov rsi,TextProtectedMode ; si = message text
 	;
 	ForEachChar:
 		;
@@ -102,10 +166,10 @@ prot_mode:
 		je EndForEachChar
         or eax, 0x0f00
 		;
-		mov [ebx],ax	; write char to display memory
+		mov [rbx],ax	; write char to display memory
 		;
-		inc ebx		; 2 bytes per char
-		inc ebx		; so increment twice
+		inc rbx		; 2 bytes per char
+		inc rbx		; so increment twice
 		;
 	jmp ForEachChar
 	EndForEachChar:
@@ -114,7 +178,7 @@ prot_mode:
 	;
 	ret
 	;
-	TextProtectedMode: db 'The processor is in protected mode. GILAD THE KING',0
+	TextProtectedMode: db 'The processor is in protected and paging 64 bit mode.',0
 
 GlobalDescriptorTable: 
 NULL_DESC: ; Not really NULL. no one use it so we use it.
@@ -145,6 +209,32 @@ gdtr:
 
 GlobalDescriptorTableEnd:
 
+GDT64:                           ; Global Descriptor Table (64-bit).
+    .Null: equ $ - GDT64         ; The null descriptor.
+    dw 0                         ; Limit (low).
+    dw 0                         ; Base (low).
+    db 0                         ; Base (middle)
+    db 0                         ; Access.
+    db 0                         ; Granularity.
+    db 0                         ; Base (high).
+    .Code: equ $ - GDT64         ; The code descriptor.
+    dw 0xFFFF                    ; Limit (low).
+    dw 0                         ; Base (low).
+    db 0                         ; Base (middle)
+    db 10011010b                 ; Access (exec/read).
+    db 00100000b                 ; Granularity.
+    db 0                         ; Base (high).
+    .Data: equ $ - GDT64         ; The data descriptor.
+    dw 0xFFFF                    ; Limit (low).
+    dw 0                         ; Base (low).
+    db 0                         ; Base (middle)
+    db 10010010b                 ; Access (read/write).
+    db 00000000b                 ; Granularity.
+    db 0                         ; Base (high).
+    .Pointer:                    ; The GDT-pointer.
+    dw $ - GDT64 - 1             ; Limit.
+    dq GDT64                     ; Base.
+    
 ;===========================================
 here1 db 'here1', 0x0D, 0x0A, 0
 
