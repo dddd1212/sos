2fbcfa100a9d379cfac3312ee44bff4fe5e161cd
giladgiladr <giladgiladr@gmail.com>
2016-03-23 00:26:19 +0200

    bootimg (kernel_loader.c) scratch

--- stage1/prekernel/kernel.h
+++ stage1/prekernel/kernel.h
@@ -1,26 +1,30 @@
 #define NULL 0
 
+#define MAX_LOADED_MODULES 0x100
+
 #define uchar unsigned char
+DWORD
+
+
+
+
 
 // gword and ugword are plateform specific. thay can be 64bit or 32bit.
 #define gword  signed long long
 #define ugword unsigned long long
-
-struct Symbol {
-    void * address;
-    uchar * name;
-    gword type;
+enum PAGE_ACCESS {
+    PAGE_ACCESS_NONE  = 0x0,
+    PAGE_ACCESS_READ  = 0x1,
+    PAGE_ACCESS_WRITE = 0x2,
+    PAGE_ACCESS_RW    = 0x3,
+    PAGE_ACCESS_EXEC  = 0x4,
+    PAGE_ACCESS_RX    = 0x5,
+    PAGE_ACCESS_WX    = 0x6,
+    PAGE_ACCESS_RWX   = 0x7,
 }
 
-struct Symbols {
-    struct Symbols * next; // pointer to the next list of symbols.
-    gword num_of_symbols; // Number of symbols in this list.
-    Symbol symbols[0]; // Undefined number of symbols.
-}
-
+typedef Eff * ModulesList[MAX_LOADED_MODULES];
 
 struct KernelGlobalData {
-    void * STAGE0_next_mem;
-    struct Symbols * syms;
-    
+    struct ModulesList * modules;
 }
--- stage1/prekernel/kernel_loader.h
+++ stage1/prekernel/kernel_loader.h
@@ -1,17 +1,5 @@
 #include "kernel.h"
 
-#define NUMBER_OF_SYMBOLS_IN_LIST 100
-
-#define ALLOCATOR_ALIGN 0x10
-
-
-struct STAGE0Stage1DriversList {
-    unsigned int size;
-    unsigned char data[0];
-}
-
-struct STAGE0SymbolTableStruct {
-    unsigned int num_of_symbols;
-    unsigned int 
-}
+#define BOOT_TXT_FILE_MAX_SIZE 0x1000
+#define MAX_BOOT_MODULES 0x10
 
