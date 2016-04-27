a0953f2e71dfb8089d6b66a0c5d72fd476dd1fc4
dddd1212 <dddd1212@gmail.com>
2016-04-28 01:00:07 +0300

    Qbject updates

--- stage1/Qube/QbjectManager/Qbject.h
+++ stage1/Qube/QbjectManager/Qbject.h
@@ -9,15 +9,31 @@ typedef union {
 	};
 } QbjectType;
 
-typedef QResult(*QbjectReadFunction)(QobjectContent, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
-typedef QResult(*QbjectWriteFunction)(QobjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
-typedef void* QobjectContent;
+//Qbject
+typedef void* QbjectContent;
+typedef QResult(*QbjectReadFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
+typedef QResult(*QbjectWriteFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
+
+//QbjectContainer
+typedef Qbject* (*QbjectOpenFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
+
 
 typedef struct {
 	QbjectType type;
+	QbjectContent content;
 	QbjectReadFunction read_func;
 	QbjectWriteFunction write_func;
-	QobjectContent content;
 } Qbject;
+
+typedef struct {
+	// same as Qbject
+	QbjectType type;
+	QbjectContent content;
+	QbjectReadFunction read_func;
+	QbjectWriteFunction write_func;
+	
+	// additional fields for containers
+	QbjectOpenFunction open;
+} QbjectContainer;
 
 #endif
