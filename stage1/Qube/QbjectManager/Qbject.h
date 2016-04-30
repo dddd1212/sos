bd455e6edb944d576acd16aec417bf118c5d957d
dddd1212 <dddd1212@gmail.com>
2016-04-30 22:37:36 +0300

    little updates

--- stage1/Qube/QbjectManager/Qbject.h
+++ stage1/Qube/QbjectManager/Qbject.h
@@ -8,15 +8,11 @@ typedef union {
 		uint32 isContainer : 1;
 	};
 } QbjectType;
-
+typedef int32 ACCESS;
 //Qbject
 typedef void* QbjectContent;
 typedef QResult(*QbjectReadFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
 typedef QResult(*QbjectWriteFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
-
-//QbjectContainer
-typedef Qbject* (*QbjectOpenFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
-
 
 typedef struct {
 	QbjectType type;
@@ -24,6 +20,10 @@ typedef struct {
 	QbjectReadFunction read_func;
 	QbjectWriteFunction write_func;
 } Qbject;
+
+
+//QbjectContainer
+typedef Qbject* (*QbjectOpenFunction)(QbjectContent container, char* path, ACCESS access);
 
 typedef struct {
 	// same as Qbject
@@ -35,5 +35,7 @@ typedef struct {
 	// additional fields for containers
 	QbjectOpenFunction open;
 } QbjectContainer;
+
+Qbject* OpenQbject(char* path, ACCESS access);
 
 #endif
