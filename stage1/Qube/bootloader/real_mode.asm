88a929b417ab86e8e905cd4f164100f95fd9d3ea
dddd1212 <dddd1212@gmail.com>
2016-05-08 01:02:22 +0300

    (1) updates in MemoryManagement (2) extend boot pages to 7

--- stage1/Qube/bootloader/real_mode.asm
+++ stage1/Qube/bootloader/real_mode.asm
@@ -117,7 +117,7 @@ prot_mode:
     
 	mov eax, 0x7003
     mov ebx, 0x22038
-	mov ecx, 6 # num of pages to map
+	mov ecx, 7 # num of pages to map
 map_boot_pages:
     mov [ebx],eax # set the PTE entry of code.
 	add eax, 0x1000
