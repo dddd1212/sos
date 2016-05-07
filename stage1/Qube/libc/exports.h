5ea0aa7007d580e0d8aab7f90b03cf8bcf05be97
giladgiladr <giladgiladr@gmail.com>
2016-05-08 01:40:43 +0300

    fix lot of bugs in loader.c

--- stage1/Qube/libc/exports.h
+++ stage1/Qube/libc/exports.h
@@ -1,3 +1,15 @@
 #include "exports_auto_gen.h"
 #define MODULE_NAME libc
 #include "../common/auto_exports.h"
+
+
+EXPORT(strcpy);
+EXPORT(strncpy);
+EXPORT(strcat);
+EXPORT(strncat);
+EXPORT(strcmp);
+EXPORT(strncmp);
+EXPORT(strlen);
+EXPORT(memset);
+EXPORT(memcpy);
+EXPORT(memcmp);
--- stage1/Qube/libc/exports_auto_gen.h
+++ stage1/Qube/libc/exports_auto_gen.h
@@ -1,1 +1,11 @@
 // Auto generated file!
+#define strcpy EXP_libc_strcpy
+#define strncpy EXP_libc_strncpy
+#define strcat EXP_libc_strcat
+#define strncat EXP_libc_strncat
+#define strcmp EXP_libc_strcmp
+#define strncmp EXP_libc_strncmp
+#define strlen EXP_libc_strlen
+#define memset EXP_libc_memset
+#define memcpy EXP_libc_memcpy
+#define memcmp EXP_libc_memcmp
