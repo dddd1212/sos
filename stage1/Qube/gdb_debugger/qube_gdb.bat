a0f788b43a9c02006907ead2b39bd39ef6292cba
dddd1212 <dddd1212@gmail.com>
2016-05-09 00:45:24 +0300

    gdb_debugger files

---
+++ stage1/Qube/gdb_debugger/qube_gdb.bat
@@ -0,0 +1,2 @@
+start "" "C:\\Program Files (x86)\\Bochs-2.6.8\\bochs_gdb.exe" -q -f D:\\dror\\projects\\SOS\\stage1\\Qube\\bootloader\\bochsrcTEST_gdb.bxrc
+D:\dror\projects\SOS\cygwinthings\bin\gdb.exe %1 %2
