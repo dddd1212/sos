7a54ddbbbe930eb269af732f9a155f2b90b92d3e
giladgiladr <giladgiladr@gmail.com>
2016-05-15 23:39:43 +0300

    move spin_lock to different file

---
+++ stage1/Qube/Common/spin_lock.h
@@ -0,0 +1,25 @@
+#ifndef __SPIN_LOCK_H__
+#define __SPIN_LOCK_H__
+
+#include "intrinsics.h""
+typedef int volatile * SpinLock;
+
+void inline spin_init(SpinLock p) {
+	*p = 0;
+}
+
+void inline spin_lock(SpinLock p)
+{
+	while (!__qube_sync_bool_compare_and_swap(p, 0, 1))
+	{
+		while (*p) __qube_mm_pause();
+	}
+}
+
+void inline spin_unlock(SpinLock p)
+{
+	__qube_memory_barrier();
+	*p = 0;
+}
+
+#endif // __SPIN_LOCK_H__
