7ede320b1e28e5d6502791d71ccc8fbbd344cd3a
dddd1212 <dddd1212@gmail.com>
2016-05-23 22:11:33 +0300

    Qbject updates

--- stage1/Qube/QbjectManager/Qbject.c
+++ stage1/Qube/QbjectManager/Qbject.c
@@ -1,6 +1,31 @@
 #include "Qbject.h"
+QNode root;
+
+QResult qkr_main(KernelGlobalData * kgd) {
+	root.type = QNODE_TYPE_ROOT;
+	root.CreateQbjectFunction = NULL;
+	root.QbjectReadFunction = NULL;
+	root.QbjectWriteFunction = NULL;
+	root.left_child = NULL;
+	root.rigth_sibling = NULL;
+}
+
 
 Qbject * OpenQbject(char * path, ACCESS access)
+{
+	char *start;
+	char *end;
+	start = end = path;
+
+	return NULL;
+}
+
+Qbject * create_qbject(char * path, ACCESS access)
+{
+	return NULL;
+}
+
+Qbject * allocate_qbject(uint32 content_size)
 {
 	return NULL;
 }
--- stage1/Qube/QbjectManager/Qbject.h
+++ stage1/Qube/QbjectManager/Qbject.h
@@ -1,35 +1,41 @@
 #ifndef QBJECT_H
 #define QBJECT_H
 #include "../Common/Qube.h"
-typedef union {
-	uint32 value;
-	struct {
-		uint32 id : 31;
-		uint32 isContainer : 1;
-	};
-} QbjectType;
-typedef int32 ACCESS;
+typedef uint32 QNodeType;
+typedef uint32 ACCESS;
 //Qbject
 typedef void* QbjectContent;
-typedef QResult(*QbjectReadFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
-typedef QResult(*QbjectWriteFunction)(QbjectContent, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
+
+typedef struct QNode_ QNode;
 
 typedef struct {
-	QbjectType type;
-	QbjectContent content;
-	QbjectReadFunction read_func;
-	QbjectWriteFunction write_func;
+	QNode* associated_qnode;
+	uint8 content[0];
 } Qbject;
 
+typedef void* QHandle;
 
 //QbjectNode
-typedef Qbject* (*QbjectOpenFunction)(QbjectContent container, char* path, ACCESS access);
 
-typedef struct {
-	// same as Qbject
-	QbjectOpenFunction open;
-} QbjectNode;
+typedef Qbject* (*CreateQbjectFunction)(char* path, ACCESS access);
+typedef QResult(*QbjectReadFunction)(Qbject* qbject, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
+typedef QResult(*QbjectWriteFunction)(Qbject* qbject, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
 
-Qbject* OpenQbject(char* path, ACCESS access);
+struct QNode_{
+	QNodeType type;
+	CreateQbjectFunction create_qbject;
+	QbjectReadFunction read;
+	QbjectWriteFunction write;
+	QNode* left_child;
+	QNode* rigth_sibling
+};
 
+QHandle create_qbject(char* path, ACCESS access);
+
+QHandle allocate_qbject(uint32 content_size);
+
+QbjectContent inline get_qbject_content(QHandle handle) {
+	return (void*)(&((Qbject*)handle)->content);
+}
+#define QNODE_TYPE_ROOT (0)
 #endif
