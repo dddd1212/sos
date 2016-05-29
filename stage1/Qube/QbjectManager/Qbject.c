2881f882431c320453a8039755cb493972e3a4ff
dddd1212 <dddd1212@gmail.com>
2016-05-30 01:06:15 +0300

    Qbject manager updates

--- stage1/Qube/QbjectManager/Qbject.c
+++ stage1/Qube/QbjectManager/Qbject.c
@@ -1,31 +1,79 @@
 #include "Qbject.h"
+#include "../MemoryManager/memory_manager.h"
+#include "../Common/spin_lock.h"
+// TODO: Use better synchronization.
 QNode root;
-
+SpinLock treelock;
 QResult qkr_main(KernelGlobalData * kgd) {
 	root.type = QNODE_TYPE_ROOT;
-	root.CreateQbjectFunction = NULL;
-	root.QbjectReadFunction = NULL;
-	root.QbjectWriteFunction = NULL;
+	root.create_qbject = NULL;
+	root.read = NULL;
+	root.write = NULL;
 	root.left_child = NULL;
 	root.rigth_sibling = NULL;
+	spin_init(treelock);
+	root.name[0] = '\0';
+}
+static BOOL name_match(QNode *qnode, char *s, uint32 len) {
+	return ((len < MAX_QNODE_NAME_LEN) && (memcmp(qnode->name, s, len) == 0) && (qnode->name[len] == '\0'));
+}
+QHandle create_qbject(char * path, ACCESS access)
+{
+	spin_lock(treelock);
+	char *start,*end;
+	start = path;
+	while (*start == '/') {
+		start++;
+	}
+	QNode *cur;
+	cur = &root;
+	QHandle h = NULL;
+	while (1) {
+		// first - try to create the Qbject using the assigned crate_qbject function if any.
+		if (cur->create_qbject) {
+			h = cur->create_qbject(start, access, 0); // first chance
+			if (h) {
+				break;
+			}
+		}
+		
+		// then try to open child
+		end = start;
+		while ((*end != '\0') && (*end != '/')) {
+			end++;
+		}
+		if (start == end) {
+			break;
+		}
+		QNode *child;
+		for (child = cur->left_child; child != NULL; child = child->rigth_sibling){
+			if (name_match(child, start, end - start)) {
+				start = end;
+				while (*start == '/') {
+					start++;
+				}
+				break;
+			}
+		}
+		if (!child) {
+			break;
+		}
+	};
+
+	if (cur->create_qbject) {
+		QHandle h = cur->create_qbject(start, access, CREATE_QBJECT_FLAGS_SECOND_CHANCE); // second chance
+	}
+
+	spin_unlock(treelock);
+
+	if (h) {
+		((Qbject*)h)->associated_qnode = cur;
+	}
+
+	return h;
 }
 
-
-Qbject * OpenQbject(char * path, ACCESS access)
+QHandle allocate_qbject(uint32 content_size)
 {
-	char *start;
-	char *end;
-	start = end = path;
-
-	return NULL;
-}
-
-Qbject * create_qbject(char * path, ACCESS access)
-{
-	return NULL;
-}
-
-Qbject * allocate_qbject(uint32 content_size)
-{
-	return NULL;
+	return kalloc(sizeof(Qbject) + content_size);
 }
--- stage1/Qube/QbjectManager/Qbject.h
+++ stage1/Qube/QbjectManager/Qbject.h
@@ -6,10 +6,10 @@ typedef uint32 ACCESS;
 //Qbject
 typedef void* QbjectContent;
 
-typedef struct QNode_ QNode;
+struct QNode_;
 
 typedef struct {
-	QNode* associated_qnode;
+	struct QNode_* associated_qnode;
 	uint8 content[0];
 } Qbject;
 
@@ -17,18 +17,19 @@ typedef void* QHandle;
 
 //QbjectNode
 
-typedef Qbject* (*CreateQbjectFunction)(char* path, ACCESS access);
+typedef Qbject* (*CreateQbjectFunction)(char* path, ACCESS access, uint32 flags);
 typedef QResult(*QbjectReadFunction)(Qbject* qbject, uint8* buffer, uint64 num_of_bytes_to_read, uint64* res_num_read);
 typedef QResult(*QbjectWriteFunction)(Qbject* qbject, uint8* buffer, uint64 num_of_bytes_to_write, uint64* res_num_written);
-
-struct QNode_{
+#define MAX_QNODE_NAME_LEN (0x20)
+typedef struct QNode_{
 	QNodeType type;
 	CreateQbjectFunction create_qbject;
 	QbjectReadFunction read;
 	QbjectWriteFunction write;
-	QNode* left_child;
-	QNode* rigth_sibling
-};
+	struct QNode_* left_child;
+	struct QNode_* rigth_sibling;
+	char name[MAX_QNODE_NAME_LEN];
+} QNode;
 
 QHandle create_qbject(char* path, ACCESS access);
 
@@ -38,4 +39,6 @@ QbjectContent inline get_qbject_content(QHandle handle) {
 	return (void*)(&((Qbject*)handle)->content);
 }
 #define QNODE_TYPE_ROOT (0)
+
+#define CREATE_QBJECT_FLAGS_SECOND_CHANCE (1)
 #endif
