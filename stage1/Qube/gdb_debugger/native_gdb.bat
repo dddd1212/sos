a19f1de94a911e9ae9bdd057a6021221079a4b50
giladgiladr <giladgiladr@gmail.com>
2016-07-03 00:12:02 +0300

    native debugger

---
+++ stage1/Qube/gdb_debugger/native_gdb.bat
@@ -0,0 +1,7 @@
+start "" "%BOCHSGDB%" -q -f bochsrc_gdb.bxrc
+sleep 1
+start gdb --eval-command="target remote localhost:1234"
+
+rem start python gdb_proxy.py %1 %2
+rem sleep 5
+rem python.exe gdb_proxy.py %1 %2
