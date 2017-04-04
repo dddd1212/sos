03eeee81c3d9af6d2e8d3aef01e785c5814a8120
dddd1212 <dddd1212@gmail.com>
2017-04-05 01:15:12 +0300

    introduce network manager

---
+++ stage1/Qube/network_manager/network_manager.c
@@ -0,0 +1,85 @@
+#include "network_manager.h"
+BOOL g_interface_registered;
+QResult qkr_main(KernelGlobalData* kgd) {
+	g_interface_registered = FALSE;
+	return QSuccess;
+}
+
+QResult empty_network_queue(NetworkQueueManagerContextI* nm_queue_context) {
+	NetworkData network_data;
+	while (nm_queue_context->is_queue_empty(nm_queue_context->driver_context)) {
+		nm_queue_context->get_network_data(nm_queue_context->driver_context, &network_data);
+	}
+}
+
+QHandle create_network_qbject(void* qnode_context, char* path, ACCESS access, uint32 flags) {
+	NetworkInterfaceManagerContextI* interface_manager_context = (NetworkInterfaceManagerContextI*)qnode_context;
+	if (interface_manager_context->qbject_created) {
+		return QFail;
+	}
+	interface_manager_context->qbject_created = TRUE;
+	QHandle qhandle = allocate_qbject(0);
+	return qhandle;
+}
+
+QResult read_network_qbject(QHandle qbject, uint8* buffer, uint64 position, uint64 num_of_bytes_to_read, uint64* res_num_read) {
+	return QFail;
+}
+
+QResult register_interface(NetworkInterfaceRegisterData* interface_data) {
+	if (g_interface_registered == TRUE) {
+		return QFail;
+	}
+	g_interface_registered = TRUE;
+	NetworkInterfaceManagerContextI* interface_manager_context = kheap_alloc(sizeof(NetworkInterfaceManagerContext));
+	interface_manager_context->driver_context = interface_data->network_interface_driver_context;
+	interface_manager_context->num_of_queues = 0;
+	interface_manager_context->qbject_created = FALSE;
+	interface_data->initialize(interface_manager_context, interface_data->network_interface_driver_context);
+	create_qnode("/Device/Network/Interfaces/Interface0");
+	QNodeAttributes qnode_attrs;
+	qnode_attrs.create_qbject = create_network_qbject;
+	qnode_attrs.get_property = NULL;
+	qnode_attrs.qnode_context= interface_manager_context;
+	qnode_attrs.read = read_network_qbject;
+	qnode_attrs.write = NULL;
+
+	set_qnode_attributes("/Device/Network/Interfaces/Interface0", &qnode_attrs);
+}
+
+QResult register_queue(NetworkInterfaceManagerContext nm_interface_context_, NetworkQueueRegisterData * queue_register_data, NetworkQueueManagerContext ** nm_queue_context_)
+{
+	NetworkInterfaceManagerContextI* nm_interface_context = (NetworkInterfaceManagerContextI*)nm_interface_context_;
+	NetworkQueueManagerContextI** nm_queue_context = (NetworkQueueManagerContextI**)nm_queue_context_;
+	if (nm_interface_context->num_of_queues >= 5) {
+		return QFail;
+	}
+	NetworkQueueManagerContextI* queue_context = kheap_alloc(sizeof(NetworkQueueManagerContextI));
+	queue_context->driver_context = queue_register_data->queue_driver_context;
+	queue_context->get_network_data = queue_register_data->get_network_data;
+	queue_context->is_queue_empty = queue_register_data->is_queue_empty;
+	queue_context->emptying_queue = FALSE;
+	spin_init(&queue_context->emptying_queue_lock);
+	queue_context->related_interface = nm_interface_context;
+	nm_interface_context->queue_list[nm_interface_context->num_of_queues] = queue_context;
+	nm_interface_context->num_of_queues++;
+	*nm_queue_context = queue_context;
+	return QSuccess;
+}
+
+QResult notify_queue_is_not_empty(NetworkQueueManagerContext nm_queue_context_)
+{
+	NetworkQueueManagerContextI* nm_queue_context = (NetworkQueueManagerContextI*)nm_queue_context_;
+	BOOL emptying_queue;
+	spin_lock(&nm_queue_context->emptying_queue_lock);
+	emptying_queue = nm_queue_context->emptying_queue;
+	spin_unlock(&nm_queue_context->emptying_queue_lock);
+	if (!emptying_queue) {
+		add_system_task((SystemTaskFunction)empty_network_queue, nm_queue_context);
+	}
+	return QSuccess;
+}
+
+QResult register_network_data_queue() {
+
+}
---
+++ stage1/Qube/network_manager/network_manager.h
@@ -0,0 +1,84 @@
+#ifndef NETWORK_MANAGER_H
+#define NETWORK_MANAGER_H
+#include "../Common/Qube.h"
+#include "../MemoryManager/memory_manager.h"
+#include "../scheduler/scheduler.h"
+#include "../Common/spin_lock.h"
+#include "../QbjectManager/Qbject.h"
+typedef void* NetworkInterfaceManagerContext;
+typedef void* NetworkQueueManagerContext;
+typedef void* NetworkInterfaceDriverContext;
+typedef void* NetworkQueueDriverContext;
+
+typedef struct NetworkData_ NetworkData;
+
+typedef QResult(*ReleaseNetworkDataFunc)(NetworkData* network_data);
+
+typedef struct NetworkBuffer_ {
+	struct NetworkBuffer* next;
+	uint8* buffer;
+	uint64 buffer_size;
+} NetworkBuffer;
+
+typedef enum {
+	ETH_RAW,
+	IPV4,
+	IPV6,
+	IPV4_TCP,
+	IPV4_UDP,
+	IPV6_TCP,
+	IPV6_UDP
+}NetworkDataType;
+
+typedef struct EthNetworkData_ {
+	NetworkBuffer* network_buffer;
+	uint64 eth_data_offset;
+} EthNetworkData;
+
+struct NetworkData_ {
+	NetworkDataType data_type;
+	ReleaseNetworkDataFunc free_network_data;
+	NetworkBuffer* first_buffer;
+	union {
+		EthNetworkData eth_data;
+	};
+};
+
+typedef QResult(*InitializeNetworkInterfaceFunc)(NetworkInterfaceManagerContext network_interface_manager_context, NetworkInterfaceDriverContext network_interface_driver_context);
+typedef QResult(*GetNetworkDataFunc)(NetworkQueueDriverContext queue_driver_context, NetworkData* out);
+typedef BOOL(*IsQueueEmptyFunc)(NetworkQueueDriverContext queue_driver_context);
+
+typedef struct NetworkQueueManagerContextI_ {
+	NetworkQueueDriverContext driver_context;
+	struct NetworkInterfaceManagerContextI_* related_interface;
+	IsQueueEmptyFunc is_queue_empty;
+	GetNetworkDataFunc get_network_data;
+	BOOL emptying_queue;
+	SpinLock emptying_queue_lock;
+} NetworkQueueManagerContextI;
+
+typedef struct NetworkInterfaceManagerContextI_ {
+	NetworkInterfaceDriverContext driver_context;
+	BOOL qbject_created;
+	uint32 num_of_queues;
+	NetworkQueueManagerContext queue_list[5]; // TODO: for now, this is the maximum number of queues, but it should not work this way.
+} NetworkInterfaceManagerContextI;
+
+
+typedef struct NetworkInterfaceRegisterData_ {
+	NetworkInterfaceDriverContext network_interface_driver_context;
+	InitializeNetworkInterfaceFunc initialize;
+} NetworkInterfaceRegisterData;
+
+typedef struct NetworkQueueRegisterData_ {
+	NetworkQueueDriverContext queue_driver_context;
+	GetNetworkDataFunc get_network_data;
+	IsQueueEmptyFunc is_queue_empty;
+} NetworkQueueRegisterData;
+
+
+EXPORT QResult register_interface(NetworkInterfaceRegisterData* interface_data);
+EXPORT QResult register_queue(NetworkInterfaceManagerContext nm_interface_context, NetworkQueueRegisterData* queue_register_data, NetworkQueueManagerContext** nm_queue_context);
+EXPORT QResult notify_queue_is_not_empty(NetworkQueueManagerContext nm_context);
+
+#endif
